f3560682eba005ec6e7cfdcf65099a642b4ba5bb
Luna <20017802+lunaboppin@users.noreply.github.com>
2025-11-09 00:44:43 +0000

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,15 +12,9 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
                     @auth
-                        <x-nav-link :href="route('pins.create')" :active="request()->routeIs('pins.create')">
-                            {{ __('Add Pin') }}
-                        </x-nav-link>
-                        <x-nav-link :href="route('pins.index')" :active="request()->routeIs('pins.index')">
-                            {{ __('View Pins') }}
+                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                            {{ __('Dashboard') }}
                         </x-nav-link>
                     @endauth
                     @guest
