831540dc742e7f57229a3b28b6a42814ca50ef6c
Luna <20017802+lunaboppin@users.noreply.github.com>
2025-11-09 00:45:38 +0000

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -72,15 +72,9 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
             @auth
-                <x-responsive-nav-link :href="route('pins.create')" :active="request()->routeIs('pins.create')">
-                    {{ __('Add Pin') }}
-                </x-responsive-nav-link>
-                <x-responsive-nav-link :href="route('pins.index')" :active="request()->routeIs('pins.index')">
-                    {{ __('View Pins') }}
+                <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    {{ __('Dashboard') }}
                 </x-responsive-nav-link>
             @endauth
             @guest
